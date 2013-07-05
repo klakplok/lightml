c3a673df4b3f474249bbb16c97f60385a6c90e49
Benjamin Canou <benjamincanou@MacBook-Pro-de-Benjamin.local>
2013-07-05 16:22:40 +0200

    code refactoring / cleaning

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ A minimalist OCaml API to construct well formed static Web sites
 - Checks the unicity of IDs inside every page (at output time)
 - Checks the absence of broken links (internal at typing time and external at output time)
 
-LightML is (C) 2013 Benjamin Canou and distributed under the terms
+**Read this:** LigHTML is for simple usecases, such as adding an HTML
+output back-end to an existing program. It provides interesting
+guarantees that can shortcut a lot of testing and debugging but its
+simplistic interface hides a lot of subtelties. For any advanced
+purpose, such as building dynamic Web sites, you may want to use TyXML
+instead [http://ocsigen.org/tyxml/manual].
+
+**License:** LightML is (C) 2013 Benjamin Canou and distributed under the terms
 of the CeCILL-B Free Software License Agreement. See the LICENSE
 file for more details. 
--- ligHTML.ml
+++ ligHTML.ml
@@ -1,59 +1,61 @@
-open Printf
+(* This file is part of LightML and is (C) 2013 Benjamin Canou. It is
+   distributed under the terms of the CeCILL-B Free Software License
+   Agreement. See the LICENSE file for more details. *)
 
+(* exported (abstract) types *)
 type page = P of (string * untyped_node list ref * string * string list ref)
-and untyped_node = S of string | T of descr * untyped_node list
-and untyped_nodes = untyped_node list
+and url = Local of string * string option * bool | Extern of string * string option
+
+(* internal types *)
 and descr = { tag : string ; attrs : attr list }
 and attr = A of string * attr_value | Href of page * string option
 and attr_value = Raw of string | Seq of string list | Url of url
-and url = Local of string * string option * bool | Extern of string * string option
+and untyped_node = S of string | T of descr * untyped_node list
+and untyped_nodes = untyped_node list
 
+(* exported types (with a phantom parameter to encode valid nesting) *)
 type 'a node = untyped_node
 type 'a nodes = untyped_nodes
 
-exception Duplicate_path of string
-exception Duplicate_id of string
-exception Unbound_id of string * string
-exception Unbound_url of string
+(* exported exceptions *)
+exception Duplicate_path of string (* path *)
+exception Duplicate_id of string (* id *)
+exception Unbound_id of string * string (* path x id *)
+exception Unbound_url of string (* url *)
 
-let local ?(probe = true) ?target url =
-  Local (url, target, probe)
+(* tools ********************************************************************)
 
-let extern ?target url =
-  Extern (url, target)
+let use_path =
+  (* TODO: normalize (remove ..s) *)
+  let module Store = Set.Make (String) in
+  let paths = ref Store.empty in
+  fun p ->
+    if Store.mem p !paths then raise (Duplicate_path p) ;
+    paths := Store.add p !paths ; p
 
-let rec collect_ids acc = function
-  | S _ -> acc
-  | T ({ attrs }, l) ->
-    let acc = List.fold_left collect_ids acc l in
-    let rec find = function
-      | A ("id", Raw id) :: tl ->
-	if List.mem id acc then
-	  raise (Duplicate_id id)
-	else
-	  id :: acc
-      | A _ :: tl ->
-	find tl
-      | Href _ :: tl -> find tl
-      | [] -> acc
-    in find attrs
-
-module Paths = struct
-  include Set.Make (String)
-  let paths = ref empty
-  let use p =
-    if mem p !paths then (
-      raise (Duplicate_path p)
-    ) else (
-      paths := add p !paths ;
-      p
-    )
-      
-  let gpuid = ref 0
-  let gen () =
+let gen_path =
+  let gpuid = ref (-1) in
+  fun () ->
     incr gpuid ;
-    sprintf "gen%06X.html" !gpuid
-end
+    Printf.sprintf "gen%06X.html" !gpuid
+
+let collect_and_check_ids root =
+  let rec traverse acc = function
+    | S _ -> acc
+    | T ({ attrs }, l) ->
+      let acc = List.fold_left traverse acc l in
+      let rec find = function
+	| A ("id", Raw id) :: tl ->
+	  if List.mem id acc then
+	    raise (Duplicate_id id)
+	  else
+	    id :: acc
+	| A _ :: tl ->
+	  find tl
+	| Href _ :: tl -> find tl
+	| [] -> acc
+      in find attrs
+  in traverse [] root
 
 let esc s =
   let buf = Buffer.create 1000 in
@@ -79,27 +81,64 @@ let esc s =
     Buffer.add_char buf ' ' ;
   Buffer.contents buf
 
+let mkdirs basedir path =
+  ((* TODO *))
+
+
+(* url builders *************************************************************)
+
+let local ?(probe = true) ?target url = Local (url, target, probe)
+let extern ?target url = Extern (url, target)
+
+(* text node builders *******************************************************)
+
 let text s = S (esc s)
 let cdata s = S ("<![CDATA[" ^ s ^ "]]>")
 let comment s = S ("<!--" ^ s ^ "-->")
 let nbsp () = S "&nbsp;"
+
+(* generic element node builders ********************************************)
+
 let elt tag ?(attrs = []) children = 
   T ({ tag ; attrs }, children)
 
-let builder tag ?id ?(cls = []) children =
+let builder ?(attrs = []) tag ?id ?(cls = []) children =
   let attrs =
     (match id with None -> [] | Some c -> [A ("id", Raw c)])
     @ (match cls with [] -> [] | l -> [A ("class", Seq l)])
+    @ attrs
   in
   elt tag ~attrs children
 
+let generic tag ?id ?(cls = []) ?(attrs = []) children =
+  let attrs = List.map (fun (n, v) -> A (n, Raw v)) attrs in
+  builder ~attrs tag ?id ~cls children
+    
+let inline = generic and block = generic
+
+(* element node builders ****************************************************)
+
 let br () = elt "br" []
 let blockquote = builder "blockquote"
-let span = builder "span"
-let div = builder "div"
-let article = builder "article"
-let section = builder "section"
+let div = builder "div" and span = builder "span"
+let article = builder "article" and section = builder "section"
 let pre ?id ?(cls = []) t = builder "pre" ?id ~cls [t]
+let ul = builder "ul" and ol = builder "ol" and li = builder "li"
+let h lvl =
+  if lvl < 1 || lvl > 6 then invalid_arg "LigHTML.h" ;
+  builder ("h" ^ string_of_int lvl)
+let h1 = h 1 and h2 = h 2 and h3 = h 3 and h4 = h 4 and h5 = h 5 and h6 = h 6
+let sub = builder "sub" and sup = builder "sup"
+let img ?id ?(cls = []) url =
+  builder ~attrs:[A ("src", Url url)] "img" ?id ~cls []
+let a_url ?id ?(cls = []) url children =
+  builder ~attrs:[A ("href", Url url)] "a" ?id ~cls children
+let a ?id ?(cls = []) ?target page children =
+  let cls = "lightml-page-ref" :: cls in
+  builder ~attrs:[Href (page, target)] "a" ?id ~cls children
+
+(* table builders ***********************************************************)
+
 let table ?id ?(cls = []) ?headers t =
   let t = match headers with None -> t | Some ths -> (builder "tr" ths) :: t in
   if t = [] then invalid_arg "LigHTML.table" ;
@@ -109,61 +148,16 @@ let tr ?id ?(cls = []) ?header t =
   if t = [] then invalid_arg "LigHTML.tr" ;
   builder ?id ~cls "tr" t
 let td = builder "td"
-let th = builder "th"
-let ul = builder "ul"
-let ol = builder "ol"
-let li = builder "li"
-let h lvl =
-  if lvl < 1 || lvl > 6 then invalid_arg "LigHTML.h" ;
-  builder ("h" ^ string_of_int lvl)
-let h1 = builder "h1"
-let h2 = builder "h2"
-let h3 = builder "h3"
-let h4 = builder "h4"
-let h5 = builder "h5"
-let h6 = builder "h6"
-let sub = builder "sub"
-let sup = builder "sup"
-let generic tag ?id ?(cls = []) ?(attrs = []) children =
-  let attrs =
-    (match id with None -> [] | Some c -> [A ("id", Raw c)])
-    @ (match cls with [] -> [] | l -> [A ("class", Seq l)])
-    @ (List.map (fun (n, v) -> A (n, Raw v)) attrs)
-  in
-  elt tag ~attrs children
-let inline = generic
-let block = generic
+and th = builder "th"
 
-let img ?id ?(cls = []) url =
-  let attrs =
-    (match id with None -> [] | Some c -> [A ("id", Raw c)])
-    @ (match cls with [] -> [] | l -> [A ("class", Seq l)])
-    @ ([A ("src", Url url)])
-  in
-  elt "img" ~attrs []
+(* main functions ***********************************************************)
 
-let a_url ?id ?(cls = []) url children =
-  let attrs =
-    (match id with None -> [] | Some c -> [A ("id", Raw c)])
-    @ (match cls with [] -> [] | l -> [A ("class", Seq l)])
-    @ ([A ("href", Url url)])
-  in
-  elt "a" ~attrs children
-
-let a ?id ?(cls = []) ?target page children =
-  let cls = "local-url" :: cls in
-  let attrs =
-    (match id with None -> [] | Some c -> [A ("id", Raw c)])
-    @ (match cls with [] -> [] | l -> [A ("class", Seq l)])
-    @ ([Href (page, target)])
-  in
-  elt "a" ~attrs children
-
-let new_page ?(path = Paths.gen ()) ?(contents = []) title : page =
-  P (title, ref contents, Paths.use path, ref [])
-
-let mkdirs basedir path =
-  ((* TODO *))
+let new_page ?(path = gen_path ()) ?(contents = []) title : page =
+  P (title, ref contents, use_path path, ref [])
+    
+let set_page_contents (P (_, contents, _, l) : page) e =
+  contents := e ;
+  l := collect_and_check_ids (elt "fake" e)
 
 let write ?(basedir = ".") (p : page) =
   let written = ref [] in
@@ -175,65 +169,63 @@ let write ?(basedir = ".") (p : page) =
       for i = 1 to if n > 0 then !lvl else !lvl + n do Buffer.add_char buf ' ' done ;
       lvl := !lvl + n
     in
-    let bprint_attrs buf =
+    let do_attrs buf =
       let target = function | None -> "" | Some t -> "#" ^ t in
       List.iter (function
-	| A (a, Raw v) -> bprintf buf " %s='%s'" a v
+	| A (a, Raw v) -> Printf.bprintf buf " %s='%s'" a v
 	| A (a, Seq []) -> ()
-	| A (a, Seq (e :: tl)) -> bprintf buf " %s='%s'" a (List.fold_left (fun r s -> r ^ " " ^ s) e tl)
-	| A (a, Url (Local (v, t, false))) -> bprintf buf " %s='%s%s'" a v (target t)
+	| A (a, Seq (e :: tl)) -> Printf.bprintf buf " %s='%s'" a (List.fold_left (fun r s -> r ^ " " ^ s) e tl)
+	| A (a, Url (Local (v, t, false))) -> Printf.bprintf buf " %s='%s%s'" a v (target t)
 	| A (a, Url (Local (v, t, true))) ->
 	  if not (Sys.file_exists (basedir ^ "/" ^ v)) then raise (Unbound_url v) ;
-	  bprintf buf " %s='%s%s'" a v (target t)
-	| A (a, Url (Extern (v, t))) -> bprintf buf " %s='%s%s'" a v (target t)
+	  Printf.bprintf buf " %s='%s%s'" a v (target t)
+	| A (a, Url (Extern (v, t))) -> Printf.bprintf buf " %s='%s%s'" a v (target t)
 	| Href (P (_, _, path, _) as p, None) ->
 	  if not (List.memq p !written) && not (List.memq p !to_write) then
 	    to_write := p :: !to_write ;
-	  bprintf buf " href='%s'" path
+	  Printf.bprintf buf " href='%s'" path
 	| Href (P (_, _, path, tids) as p, Some id) ->
 	  if not (List.mem id !tids) then
 	    raise (Unbound_id (path, id)) ;
 	  if not (List.memq p !written) && not (List.memq p !to_write) then
 	    to_write := p :: !to_write ;
-	  bprintf buf " href='%s#%s'" path id
+	  Printf.bprintf buf " href='%s#%s'" path id
       ) in
-    let rec aplat = function
+    let rec inlineable = function
       | S s -> true
       | T (_, [S s]) -> true
       | T (_, l) when List.exists (function | S _ | T({ tag = ""},_) -> true | _ -> false) l -> true
       | T (_, [T (_, [S s])]) when String.length s < 30 -> true
       | _ -> false
     in
-    let rec aux a = function
+    let rec do_node ?(inline = false) = function
       | S s ->
-	if a then
-	  bprintf buf "%s" s
+	if inline then
+	  Printf.bprintf buf "%s" s
 	else
-	  bprintf buf "%a%s\n" indent 0 s
+	  Printf.bprintf buf "%a%s\n" indent 0 s
       | T ({ tag ; attrs }, []) ->
-	if a then
-	  bprintf buf "<%s%a></%s>" tag bprint_attrs attrs tag
+	if inline then
+	  Printf.bprintf buf "<%s%a></%s>" tag do_attrs attrs tag
 	else
-	  bprintf buf "%a<%s%a></%s>\n" indent 0 tag bprint_attrs attrs tag
+	  Printf.bprintf buf "%a<%s%a></%s>\n" indent 0 tag do_attrs attrs tag
       | T ({ tag ; attrs }, l) as e ->
-	let ae = a || aplat e in
-	if a then (
-	  bprintf buf "<%s%a>" tag bprint_attrs attrs ;
-	  List.iter (aux true) l ;
-	  bprintf buf "</%s>" tag
+	if inline then (
+	  Printf.bprintf buf "<%s%a>" tag do_attrs attrs ;
+	  List.iter (do_node ~inline:true) l ;
+	  Printf.bprintf buf "</%s>" tag
+	) else if inlineable e then (
+	  Printf.bprintf buf "%a<%s%a>" indent 0 tag do_attrs attrs ;
+	  List.iter (do_node ~inline:true) l ;
+	  Printf.bprintf buf "</%s>\n" tag
 	) else (
-	  if ae then (
-	    bprintf buf "%a<%s%a>" indent 0 tag bprint_attrs attrs ;
-	    List.iter (aux true) l ;
-	    bprintf buf "</%s>\n" tag
-	  ) else (
-	    bprintf buf "%a<%s%a>\n" indent 1 tag bprint_attrs attrs ;
-	    List.iter (aux false) l ;
-	    bprintf buf "%a</%s>\n" indent (-1) tag
-	  )
+	  Printf.bprintf buf "%a<%s%a>\n" indent 1 tag do_attrs attrs ;
+	  List.iter do_node l ;
+	  Printf.bprintf buf "%a</%s>\n" indent (-1) tag
 	)
     in
-    let e = elt "html" [
+    let root = elt "html" [
+      (* TODO: customize headers *)
       elt "head" [
 	elt "meta" ~attrs:[A ("http-equiv", Raw "content-type") ; A ("content", Raw "text/html; charset=utf-8")] [] ;
 	elt "meta" ~attrs:[A ("name", Raw "robots") ; A ("content", Raw "index, follow")] [] ;
@@ -244,7 +236,7 @@ let write ?(basedir = ".") (p : page) =
       ] ;
       elt "body" !contents
     ] in
-    aux false e ;
+    do_node root ;
     let t = Buffer.contents buf in
     mkdirs basedir path ;
     let fp = open_out (basedir ^ "/" ^ path) in
@@ -258,7 +250,3 @@ let write ?(basedir = ".") (p : page) =
     to_write := List.tl !to_write ;
     do_page title contents path
   done
-    
-let set_page_contents (P (_, contents, _, l) : page) e =
-  contents := e ;
-  l := collect_ids [] (T ({ tag = "" ; attrs = [] }, e))
